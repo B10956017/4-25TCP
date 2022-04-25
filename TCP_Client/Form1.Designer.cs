7618422f5235013464b9e05caa610eaa0339c99e
user <user@DESKTOP-6JBEF5K>
2022-04-25 11:41:58 +0800

    練習完成

--- TCP_Client/Form1.Designer.cs
+++ TCP_Client/Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿
-namespace WindowsFormsApp1
+namespace TCP_Client
 {
     partial class Form1
     {
@@ -29,99 +29,95 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(91, 78);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(246, 268);
-            this.listBox1.TabIndex = 0;
-            // 
             // textBox1
             // 
-            this.textBox1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(585, 69);
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(184, 57);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(154, 36);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox2.Location = new System.Drawing.Point(602, 159);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(137, 36);
-            this.textBox2.TabIndex = 2;
+            this.textBox1.Size = new System.Drawing.Size(136, 40);
+            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(585, 283);
+            this.button1.Location = new System.Drawing.Point(390, 168);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 63);
+            this.button1.Size = new System.Drawing.Size(294, 65);
             this.button1.TabIndex = 3;
-            this.button1.Text = "啟動伺服器";
+            this.button1.Text = "登入伺服器";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox2.Location = new System.Drawing.Point(535, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(149, 40);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox3.Location = new System.Drawing.Point(184, 184);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 40);
+            this.textBox3.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(86, 29);
+            this.label1.Location = new System.Drawing.Point(51, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 27);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "線上使用者";
+            this.label1.Size = new System.Drawing.Size(115, 27);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "伺服器IP";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(482, 72);
+            this.label2.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(385, 60);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(97, 24);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Server IP";
+            this.label2.Size = new System.Drawing.Size(135, 27);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "伺服器Port";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label3.Location = new System.Drawing.Point(482, 162);
+            this.label3.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(51, 187);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 24);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Server Port";
+            this.label3.Size = new System.Drawing.Size(120, 27);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "玩家名稱";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1112, 620);
+            this.ClientSize = new System.Drawing.Size(948, 668);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
-            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
-            this.Text = "TCP_Server伺服器端";
+            this.Text = "TCP_Client客戶端";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,10 +125,10 @@ namespace WindowsFormsApp1
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,7 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
@@ -15,6 +18,77 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        TcpListener Server;//伺服器端網路監聽器
+        Socket Client;//給客戶用的連線物件
+        Thread Th_Svr;//伺服器監聽用執行緒
+        Thread Th_Cli;//客戶端用通話執行緒
+        Hashtable HT = new Hashtable();//
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Th_Svr = new Thread(ServerSub);
+            Th_Svr.IsBackground = true;
+            Th_Svr.Start();
+            button1.Enabled = false;
+        }
+        //接收客戶端連線要求的方法，針對每一客戶建立一個連線及獨立執行緒
+        private void ServerSub()
+        {
+            IPEndPoint EP = new IPEndPoint(IPAddress.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            Server = new TcpListener(EP);//建立server端監聽器
+            Server.Start(100);//最多100人連線
+            while(true)
+            {
+                Client = Server.AcceptSocket();//建立客戶連線物件
+                Th_Cli = new Thread(Listen);//建立客戶連線獨立的執行緒
+                Th_Cli.IsBackground = true;
+                Th_Cli.Start();
+            }
+        }
+        private void Listen()
+        {
+            Socket Sck = Client;
+            Thread Th = Th_Cli;
+            while(true)
+            {
+                try
+                {
+                    byte[]B = new byte[1023];
+                    int inLen = Sck.Receive(B);
+                    string Msg = Encoding.Default.GetString(B, 0, inLen);
+                    string Cmd = Msg.Substring(0, 1);
+                    string Str = Msg.Substring(1);
+                    switch(Cmd)
+                    {
+                        case "0":
+                            HT.Add(Str, Sck);
+                            listBox1.Items.Add(Str);
+                            break;
+                        case "9":
+                            HT.Remove(Str);
+                            listBox1.Items.Remove(Str);
+                            Th.Abort();
+                            break;
+                    }
+                }
+                catch(Exception)
+                {
+                    //忽略錯誤
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.ExitThread();//關閉所有執行緒
+
         }
     }
 }
